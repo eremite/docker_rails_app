3ead6d40422828b9753b51af4bdecb5877cc2c35
Daniel Esplin <daniel@custombit.com>
2014-05-29 22:52:59 +0000

    Also support dbloading postgres.

--- docker_rails_app.sh
+++ docker_rails_app.sh
@@ -40,9 +40,19 @@ if [ $command = "bash" ]; then # bash
 fi
 
 if [ $command = "dbload" ]; then # dbload
-  cp ~/dbload/dbload.sh .
-  cp gitignore/db.sql .
-  docker run -i --rm $db_link -v $directory:/app $app ./dbload.sh
-  rm dbload.sh
-  rm db.sql
+  if [ $db = "mysql" ]; then
+    cp ~/dbload/dbload.sh .
+    cp gitignore/db.sql .
+    docker run -i --rm $db_link -v $directory:/app $app ./dbload.sh
+    rm dbload.sh
+    rm db.sql
+  else
+    echo "#!/bin/sh" > dbload.sh
+    echo "/usr/bin/psql $app --username=docker --host=db -t -c 'drop schema public cascade; create schema public;'" >> dbload.sh
+    echo "/usr/bin/pg_restore --username=docker --host=db --no-acl --no-owner --jobs=2 --dbname=$app db.dump" >> dbload.sh
+    chmod +x dbload.sh
+    cp gitignore/db.dump .
+    docker run -i --rm $db_link -v $directory:/app -e 'PGPASSWORD=docker' $app /app/dbload.sh
+    rm dbload.sh db.dump
+  fi
 fi

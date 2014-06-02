28e2d2495db35b14ffc761372fb28707533e37c6
Daniel Esplin <daniel@custombit.com>
2014-06-02 22:17:53 +0000

    Fix rails version checking and add $extra.
    And a few other improvements.

--- docker_rails_app.sh
+++ docker_rails_app.sh
@@ -2,14 +2,14 @@
 
 file=.env && test -f $file && source $file
 
-if [ -n "$app" ]; then
-  echo $app
-else
+if [ -z "$app" ]; then
   echo 'No app found.'
   exit 1
 fi
 
-db_link="--link $db:db"
+if [ -z "$db_link" ]; then
+  db_link="--link $db:db"
+fi
 
 command="$1"
 shift
@@ -19,41 +19,41 @@ if [ $command = "b" ]; then # build
 fi
 
 if [ $command = "r" ]; then # rails
-  if [ ${rails_version%.*} = "2" ]; then
+  if [ -n "$rails_version" ] && [ ${rails_version%.*} = "2" ]; then
     executable=ruby
   else
     executable=rails
   fi
-  docker run -i --rm $db_link -v $directory:/app --entrypoint /usr/local/bin/bundle $app exec $executable "$@"
+  docker run -i --rm $db_link -v $directory:/app $extra --entrypoint /usr/local/bin/bundle $app exec $executable "$@"
   find . \! -user vagrant | xargs -I % sh -c 'sudo chmod g+w %; sudo chown vagrant:vagrant %'
 fi
 
 if [ $command = "s" ]; then # rails server
-  if [ ${rails_version%.*} = "2" ]; then
+  if [ -n "$rails_version" ] && [ ${rails_version%.*} = "2" ]; then
     executable='ruby ./script/server'
   else
     executable='rails server'
   fi
-  docker run -i --rm $db_link -p 3000:3000 -v $directory:/app --entrypoint /usr/local/bin/bundle $app exec $executable
+  docker run -i --rm $db_link -p 3000:3000 -v $directory:/app $extra --entrypoint /usr/local/bin/bundle $app exec $executable
 fi
 
 if [ $command = "k" ]; then # rake
-  docker run -i --rm $db_link -v $directory:/app --entrypoint /usr/local/bin/bundle $app exec rake "$@"
+  docker run -i --rm $db_link -v $directory:/app $extra --entrypoint /usr/local/bin/bundle $app exec rake "$@"
 fi
 
 if [ $command = "t" ]; then # test
-  docker run -i --rm $db_link -v $directory:/app --entrypoint /usr/local/bin/bundle $app exec guard
+  docker run -i --rm $db_link -v $directory:/app $extra --entrypoint /usr/local/bin/bundle $app exec guard
 fi
 
 if [ $command = "bash" ]; then # bash
-  docker run -i --rm $db_link -v $directory:/app $app /bin/bash -i
+  docker run -i --rm $db_link -v $directory:/app $extra $app /bin/bash -i
 fi
 
 if [ $command = "dbload" ]; then # dbload
   if [ $db = "mysql" ]; then
     cp ~/dbload/dbload.sh .
     cp gitignore/db.sql .
-    docker run -i --rm $db_link -v $directory:/app $app ./dbload.sh
+    docker run -i --rm $db_link -v $directory:/app $extra $app ./dbload.sh
     rm dbload.sh
     rm db.sql
   else
@@ -62,7 +62,7 @@ if [ $command = "dbload" ]; then # dbload
     echo "/usr/bin/pg_restore --username=docker --host=db --no-acl --no-owner --jobs=2 --dbname=$app db.dump" >> dbload.sh
     chmod +x dbload.sh
     cp gitignore/db.dump .
-    docker run -i --rm $db_link -v $directory:/app -e 'PGPASSWORD=docker' $app /app/dbload.sh
+    docker run -i --rm $db_link -v $directory:/app -e 'PGPASSWORD=docker' $extra $app /app/dbload.sh
     rm dbload.sh db.dump
   fi
 fi

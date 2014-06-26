cb9164ff272e0f9e18fa4aa9de76634cf5b62dc4
Daniel Esplin <daniel@custombit.com>
2014-06-26 14:56:14 -0600

    Lots of improvements to database handling.

--- docker_rails_app.sh
+++ docker_rails_app.sh
@@ -2,13 +2,27 @@
 
 file=.docker_environment && test -f $file && source $file
 
+db_directory='/home/vagrant'
+db_username='docker'
+db_password='docker'
+
 if [ -z "$app" ]; then
   echo 'No app found.'
   exit 1
 fi
 
+# docker inspect --format=' ' $db
+# if [ $? -ne 0 ]; then
+if [ $db = "mysql" ]; then
+  db_username='admin'
+  docker start mysql || docker run -d --name=mysql -p 3306:3306 -v $db_directory/mysql:/var/lib/mysql -e MYSQL_PASS="$db_password" tutum/mysql
+fi
+if [ $db = "postgresql" ]; then
+  docker start postgresql || docker run -d --name=postgresql -p 5432:5432 -v $db_directory/postgresql:/data -e POSTGRESQL_USER=$db_username -e POSTGRESQL_PASS=$db_password kamui/postgresql
+fi
+
 if [ -z "$db_link" ]; then
-  db_link="--link $db:db"
+  db_link="--link $db:db -e DB_NAME=$app -e DB_USERNAME=$db_username -e DB_PASSWORD=$db_password"
 fi
 
 command="$1"
@@ -29,6 +43,7 @@ if [ $command = "r" ]; then # rails
   else
     executable=rails
   fi
+  echo "docker run -i --rm $db_link -v $directory:/app $extra --entrypoint /usr/local/bin/bundle $app exec $executable \"$@\""
   docker run -i --rm $db_link -v $directory:/app $extra --entrypoint /usr/local/bin/bundle $app exec $executable "$@"
   find . \! -user vagrant | xargs -I % sh -c 'sudo chmod g+w %; sudo chown vagrant:vagrant %'
 fi
@@ -43,7 +58,8 @@ if [ $command = "s" ]; then # rails server
   if [ -n "$running_server_id" ]; then
     docker stop $running_server_id
   fi
-  rm -f $directory/tmp/pids/server.pid
+  sudo rm -f $directory/tmp/pids/server.pid
+  echo "docker run -i --rm $db_link -p 3000:3000 -v $directory:/app $extra --entrypoint /usr/local/bin/bundle $app exec $executable"
   docker run -i --rm $db_link -p 3000:3000 -v $directory:/app $extra --entrypoint /usr/local/bin/bundle $app exec $executable
 fi
 
@@ -60,19 +76,25 @@ if [ $command = "bash" ]; then # bash
 fi
 
 if [ $command = "dbload" ]; then # dbload
+  echo "#!/bin/sh" > dbload.sh
+  chmod +x dbload.sh
   if [ $db = "mysql" ]; then
-    cp ~/dbload/dbload.sh .
+    mysql_connection="mysql -uadmin -pdocker -h db $app"
+    echo "for table in \$($mysql_connection -e 'show tables' | awk '{ print \$1}' | grep -v '^Tables')" >> dbload.sh
+    echo "do" >> dbload.sh
+    echo "  $mysql_connection -e \"drop table \$table\"" >> dbload.sh
+    echo "done" >> dbload.sh
+    echo "$mysql_connection < db.sql" >> dbload.sh
     cp gitignore/db.sql .
     docker run -i --rm $db_link -v $directory:/app $extra $app ./dbload.sh
-    rm dbload.sh
     rm db.sql
   else
-    echo "#!/bin/sh" > dbload.sh
-    echo "/usr/bin/psql $app --username=docker --host=db -t -c 'drop schema public cascade; create schema public;'" >> dbload.sh
-    echo "/usr/bin/pg_restore --username=docker --host=db --no-acl --no-owner --jobs=2 --dbname=$app db.dump" >> dbload.sh
+    echo "/usr/bin/psql $app --username=$DB_USERNAME --host=db -t -c 'drop schema public cascade; create schema public;'" >> dbload.sh
+    echo "/usr/bin/pg_restore --username=$DB_USERNAME --host=db --no-acl --no-owner --jobs=2 --dbname=$app db.dump" >> dbload.sh
     chmod +x dbload.sh
     cp gitignore/db.dump .
-    docker run -i --rm $db_link -v $directory:/app -e 'PGPASSWORD=docker' $extra $app /app/dbload.sh
-    rm dbload.sh db.dump
+    docker run -i --rm $db_link -v $directory:/app -e PGPASSWORD=$DB_PASSWORD $extra $app /app/dbload.sh
+    rm db.dump
   fi
+  rm dbload.sh
 fi

cb6c25104fecc1daef1b3481b9bc5995020996d0
Daniel Esplin <daniel@custombit.com>
2014-07-22 14:28:46 -0600

    Add dbfetch for retrieving database dumps.
    And fix password for dbload mysql.

--- docker_rails_app.sh
+++ docker_rails_app.sh
@@ -5,6 +5,7 @@ file=.docker_environment && test -f $file && source $file
 db_directory='/home/vagrant'
 db_username='docker'
 db_password='docker'
+gitignore_directory='gitignore'
 
 function fix_file_permissions {
   find . \! -user vagrant -print0 | xargs -0 -I % sh -c 'sudo chmod g+w "%"; sudo chown vagrant:vagrant "%"'
@@ -91,24 +92,51 @@ if [ $command = "bash" ]; then # bash
   docker run -i --rm $db_link -v $directory:/app $extra $app /bin/bash -i
 fi
 
+if [ $command = "dbfetch" ]; then # dbfetch
+  mkdir -p $gitignore_directory
+  if grep -q heroku .git/config; then
+    if [ $1 ]; then
+      remote="$1"
+    else
+      remote='staging'
+    fi
+    heroku pgbackups:capture -r $remote
+    curl -o $gitignore_directory/db.dump `heroku pgbackups:url -r $remote`
+  else
+    if [ $1 ]; then
+      server=$1
+    else
+      server=$app
+    fi
+    scp daniel@$server:~/db.sql.gz $gitignore_directory/
+  fi
+  if [ -e $gitignore_directory/db.sql.gz ]; then
+    cp $gitignore_directory/db.sql.gz $gitignore_directory/db.$(date +"%Y.%m.%d").sql.gz
+    gunzip -f $gitignore_directory/db.sql.gz
+  fi
+  if [ -e $gitignore_directory/db.dump ]; then
+    cp $gitignore_directory/db.dump $gitignore_directory/db.$(date +"%Y.%m.%d").dump
+  fi
+fi
+
 if [ $command = "dbload" ]; then # dbload
   echo "#!/bin/sh" > dbload.sh
   chmod +x dbload.sh
   if [ $db = "mysql" ]; then
-    mysql_connection="mysql -u $db_username -p $db_password -h db $app"
+    mysql_connection="mysql -u $db_username -p'$db_password' -h db $app"
     echo "for table in \$($mysql_connection -e 'show tables' | awk '{ print \$1}' | grep -v '^Tables')" >> dbload.sh
     echo "do" >> dbload.sh
     echo "  $mysql_connection -e \"drop table \$table\"" >> dbload.sh
     echo "done" >> dbload.sh
     echo "$mysql_connection < db.sql" >> dbload.sh
-    cp gitignore/db.sql .
+    cp $gitignore_directory/db.sql .
     docker run -i --rm $db_link -v $directory:/app $extra $app ./dbload.sh
     rm db.sql
   else
     echo "/usr/bin/psql $app --username=$db_username --host=db -t -c 'drop schema public cascade; create schema public;'" >> dbload.sh
     echo "/usr/bin/pg_restore --username=$db_username --host=db --no-acl --no-owner --jobs=2 --dbname=$app db.dump" >> dbload.sh
     chmod +x dbload.sh
-    cp gitignore/db.dump .
+    cp $gitignore_directory/db.dump .
     docker run -i --rm $db_link -v $directory:/app -e PGPASSWORD=$db_password $extra $app /app/dbload.sh
     rm db.dump
   fi
